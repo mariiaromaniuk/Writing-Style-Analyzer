8077c31651c5d269bb867bdafabd6b51b7d9619e
mariiaromaniuk <41880331+mariiaromaniuk@users.noreply.github.com>
2019-12-19 13:46:19 -0500

    Update BSTree.cpp

--- BSTree.cpp
+++ BSTree.cpp
@@ -19,16 +19,33 @@ BSTree<T>::~BSTree(){
     deleteTree(root);
 }
 
+// Copy Constructor
+template <class T>
+BSTree<T>::BSTree(const BSTree<T> & otherRoot){
+    if (otherRoot == nullptr)
+        root = nullptr;
+    else
+        copyTree(root, otherRoot);
+}
+
+// Overloading assignment operator
+template <class T>
+BSTree<T>& BSTree<T>::operator=(const BSTree<T>& otherRoot){
+    if (this != & otherRoot) {
+        if (root != nullptr)
+            deleteTree(root);
+        if (otherRoot == nullptr)
+            root = nullptr;
+        else
+            copyTree(root, otherRoot);
+    }
+    return *this;
+}
+
 // Dummy function to pass the root to preOrder
 template <class T>
 void BSTree<T>::preOrderDummy(){
     preOrder(root);
-}
-
-// Dummy function to pass the root to postOrder
-template <class T>
-void BSTree<T>::postOrderDummy(){
-    postOrder(root);
 }
 
 // Dummy function to pass the root to inOrder
@@ -37,53 +54,16 @@ void BSTree<T>::inOrderDummy(){
     inOrder(root);
 }
 
+// Dummy function to pass the root to postOrder
+template <class T>
+void BSTree<T>::postOrderDummy(){
+    postOrder(root);
+}
+
 // Dummy function to pass the root to search
 template <class T>
 bool BSTree<T>::searchDummy(T item){
     search(item,root);
-}
-
-// Function that prints out each node's data in preorder
-template <class T>
-void BSTree<T>::preOrder(Node<T>* p){
-    if (p != nullptr){
-        cout << p->info << endl;
-        preOrder(p->left);
-        preOrder(p->right);
-    }
-}
-
-// Function that prints out each node's data in postorder
-template <class T>
-void BSTree<T>::postOrder(Node<T>* p){
-    if (p != nullptr){
-        postOrder(p->left);
-        postOrder(p->right);
-        cout << p->info << endl;
-    }
-}
-
-// Function that prints out each node's data in order
-template <class T>
-void BSTree<T>::inOrder(Node<T>* p){
-    if (p != nullptr){
-        inOrder(p->left);
-        cout << p->info << endl;
-        inOrder(p->right);
-    }
-}
-
-// Function that search item in the tree
-template <class T>
-bool BSTree<T>::search(T item, Node<T>* p) {
-    if (p == nullptr)
-        return false;
-    if (p->info == item)
-        return true;
-    if (p->info < item)
-        search(item, p->right);
-    if (p->info > item)
-        search(item, p->right);
 }
 
 // Function that inserts item to the tree
@@ -118,6 +98,80 @@ void BSTree<T>::insert(T item){
         else
             r->left = temp;
     }
+}
+
+// Function that return the height of the tree
+template <class T>
+int BSTree<T>::treeHeight(){
+    return height(root);
+}
+
+// Function to check if tree is empty
+template <class T>
+bool BSTree<T>::isEmpty(){
+    return (root == nullptr);
+}
+
+// Dummy function to pass the root to deleteTree
+template <class T>
+void BSTree<T>::deleteTreeDummy(){
+    return deleteTree(root);
+}
+
+// Copy Tree
+template <class T>
+void BSTree<T>::copyTree(Node<T>* &root, Node<T>* otherRoot){
+    if (otherRoot == nullptr)
+        root = nullptr;
+    else {
+        root = new Node<T>;
+        root->info = otherRoot->info;
+        copyTree(root->left, otherRoot->left);
+        copyTree(root->right, otherRoot->right);
+    }
+}
+
+// Function that prints out each node's data in preorder
+template <class T>
+void BSTree<T>::preOrder(Node<T>* p){
+    if (p != nullptr){
+        cout << p->info << endl;
+        preOrder(p->left);
+        preOrder(p->right);
+    }
+}
+
+// Function that prints out each node's data in order
+template <class T>
+void BSTree<T>::inOrder(Node<T>* p){
+    if (p != nullptr){
+        inOrder(p->left);
+        cout << p->info << endl;
+        inOrder(p->right);
+    }
+}
+
+// Function that prints out each node's data in postorder
+template <class T>
+void BSTree<T>::postOrder(Node<T>* p){
+    if (p != nullptr){
+        postOrder(p->left);
+        postOrder(p->right);
+        cout << p->info << endl;
+    }
+}
+
+// Function that search item in the tree
+template <class T>
+bool BSTree<T>::search(T item, Node<T>* p) {
+    if (p == nullptr)
+        return false;
+    if (p->info == item)
+        return true;
+    if (p->info < item)
+        search(item, p->right);
+    if (p->info > item)
+        search(item, p->right);
 }
 
 // Function that deletes tree
